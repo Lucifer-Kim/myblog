bbb97d2559c2d708b7386b1b40ba7490f1bb9e1e
minji <minji@beatpacking.com>
2016-06-09 11:01:20 +0900

    LION13 배포

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^about$', views.about, name='about'),
-    url(r'^$', views.index, name='index'),
+    #url(r'^about$', views.about, name='about'),
+    #url(r'^$', views.index, name='index'),
+    url(r'^$', views.about, name='about'),
 ]
